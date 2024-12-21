bb60b2a467c197282d374dafec7e3b1ce510aa9d
Maximilian Kienitz <max@kienitz.dev>
2024-12-21 12:16:14 +0100

    feat(rust): add pre-commit, tree-fmt, devshell

--- rust/flake.nix
+++ rust/flake.nix
@@ -2,47 +2,84 @@
   description = "A minimal rust development flake";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     devshell = {
       url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nci.url = "github:yusdacra/nix-cargo-integration";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nci = {
+      url = "github:yusdacra/nix-cargo-integration";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
 
       imports = [
         inputs.devshell.flakeModule
         inputs.nci.flakeModule
+        inputs.pre-commit-hooks.flakeModule
+        inputs.treefmt-nix.flakeModule
       ];
 
-      perSystem = {
-        pkgs,
-        config,
-        ...
-      }: let
-        crateName = "my_crate";
-        projectName = crateName;
-        crateOutput = config.nci.outputs.${crateName};
-      in {
-        formatter = pkgs.alejandra;
-        nci = {
-          projects.${projectName}.path = ./.;
-          crates.${crateName} = {};
+      perSystem =
+        {
+          pkgs,
+          config,
+          ...
+        }:
+        let
+          crateName = "my_crate";
+          projectName = crateName;
+          crateOutput = config.nci.outputs.${crateName};
+        in
+        {
+          devshells.default = {
+            packages = [
+              pkgs.nil
+              pkgs.rust-analyzer
+            ];
+            devshell.startup.pre-commit.text = config.pre-commit.installationScript;
+          };
+
+          pre-commit.settings.hooks.treefmt.enable = true;
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              deadnix.enable = true;
+              statix.enable = true;
+              nixfmt.enable = true;
+              rustfmt.enable = true;
+            };
+          };
+
+          nci = {
+            projects.${projectName} = {
+              path = ./.;
+              numtideDevshell = "default";
+            };
+            crates.${crateName} = { };
+          };
+
+          packages.default = crateOutput.packages.release;
         };
-        devShells.default = crateOutput.devShell.overrideAttrs (old: {
-          nativeBuildInputs =
-            (with pkgs; [
-              nil
-              rust-analyzer
-            ])
-            ++ old.nativeBuildInputs;
-        });
-        packages.default = crateOutput.packages.release;
-      };
     };
 }
